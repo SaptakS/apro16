93c4200262537a280a6f1ba4c9fcb90ad23f0574
Faheem Zunjani <faheemzunjani@gmail.com>
2016-03-09 23:45:01 +0530

    Add event_desc_format.js for event descriptions

---
+++ event_desc_format.js
@@ -0,0 +1,97 @@
+var events = {
+	'Android-Quiz':{
+						'rule':'1.There will be two rounds(First Round - Question Answer, Second Round - buzzer round)<br>2.Time limit will be 15-20 minutes for each round<br>3.No external help is allowed<.br>4.Team of maximum 2 members',
+						'description':'Show your android knowledge!',
+						'contacts':'Kumar Sanyam(IIT2015121)<br>Naz Tabassum(ICM2014504)<br>Upasana Singh(IIT2014053)'
+					},
+	'Fb-Sharing-Contest':{
+						'rule':'1.You can upload any number of jokes, memes, trolls, gif.',
+						'description':'Share tech jokes, memes, trolls, gifs.<br> Number of shares or likes will determine the winner.',
+						'contacts':'Naz Tabassum(ICM2014504)<br>Upasana Singh(IIT2014053)'
+					},
+	'Black-Box':{
+						'rule':'1.	This competition is open for all students who are interested in electronics.<br>2.	There is no restriction for participation.<br>3.	This is an individual event.<br>4.	Solution to the given problem should be submitted in the form of project file generated on any of the following simulators - MULTISIM, PROTEUS or you can draw the circuit on a paper and send us the snapshot of the solution.<br>5.	DON\'T USE MICROCONTROLLERS AND MICROPROCESSORs in your circuit.<br>6.	The marks will be allotted according to the correctness and execution of the logic, time efficiency and power consumption of the circuit submitted.<br>7.	Your solution should have a brief description about the working of your circuit failing to which no marks will be allotted. The description of the circuit should be given in a text file which should be zipped along with the project file/snapshot of the solution.<br>8.	In case of multiple solutions, last solution submitted will be evaluated.<br>9.	Decision of the organizers would be final.<br>',
+						'description':'An Electronic circuit design event. It is an event that will be up for two days (i.e. 3rd & 4th April, 2016). There will be 6 questions in total. On each day you will be given 3 questions and the questions will be live at 12:00 am, 8:00 am and 12:00 pm of the day. Submissions for all the three problems will be open till midnight of that day. All the submissions are to be mailed at ‘’blackbox.aparoksha16@gmail.com’’. The filename should follow this format:  ‘’username_questionNumber.zip’’. Only Single thread is to be created (Any changes in previous answer can be made by replying to the original answer).',
+						'contacts':'Adithya Shasa Sai (8303785168)<br>Nikhil Sowrya (8953323911)<br>Vineel  Reddy (8090297395)'
+					},
+	'Blind-War':{
+						'rule':'<u>1st round:</u><ol><li>Participant has to write the program on computer without errors with monitor switch off.</li><li>The candidate should aim to successfully write the code in minimum amount of time with minimum errors.</li><li>10 candidates with 100% compiled program with correct output will be promoted to the second round.In case no one gets the correct  output  finally marks will be given on basis of the ‘quality’ of the program and number of errors.</li><li>Participant will have to code the program by turning off monitor within a given time.</li><li></ol><br><u>2nd round:</u><ol><li>Rules for second round will be same as that of the the first one but the level of complexity of the code will be higher and accordingly time limit is increased.</li><li>No papers, pendrives and codes are allowed.</li></ol>',
+						'description':'Participant will have to write a code in the computer with the screen of the monitor closed in a certain time interval. There will be 2 rounds and the winners are declared based on their outputs,time taken. In any case of clash there will be an extra round.',
+						'contacts':'IIT2013042 - K Bhagya raj - 7897009371<br>IEC2014098 - Abhinaya Kurnala - 8601732674<br>IIT2013114 - A Rahul - 8176026018<br>IIT2013199 - RSN Rohith'
+					},
+	'Language-Brainstorming':{
+						'rule':'1. It\'s an individual event.<br>2. Language will be provided on the spot and relevent tutorials will also be given.<br>3. Questionare will be given along with the tutorials.<br>4. systems will be provided.',
+						'description':'Candiates will be required to solve a set of problems based on random language using the relevent tutorials provided. The questions will cover the basic understanding of the language and it\'s implementation.',
+						'contacts':'K. BhagyaRaj - IIT2013042 - 7897009371<br>Aditya Vardhan - IIT2013044 - 7084426314<br>Osama Siddiqui - IIT2013023 - 8960448482'
+					},
+	'Ex-Machina':{
+						'rule':'To be declared soon.',
+						'description':'\‘Ex-Machina\’ is an open to all machine learning based coding competition with HackerEarth as the platform. Interested folk can participate from any and everywhere. Points are awarded for each problem solved correctly and thus the contestant with the most points will be the undoubted winner.<br>Refer to the Facebook page and the Aparoksha website ( https://aparoksha.iiita.ac.in/ ) for  further details.',
+						'contacts':'Akash Dubey - IIT2013141'
+					},
+	'Flappy-Bird-Championship':{
+						'rule':'1. If the player touches the pipes, they LOSE.<br>2. Faby briefly flaps upward each time that the player taps the screen; if the screen is not tapped, Faby falls because of gravity.<br>3. Each pair of pipes that he navigates between earns the player a single point.<br>4. Each player will be given three chances and we will be counting his best score of those.<br>5. The more points the player gets based on the competition we will be awarding 1st, 2nd and 3rd positions. In case there is tie for any position we will be given one more Chance and calculating the score again.<br>6. There was no variation or evolution in gameplay throughout the game, as the pipes always have the same gap between them and there is no end to the running track, having only the flap and ding sounds and the rising score as rewards.<br>Note :: You are allowed to bring your own mobile with game.',
+						'description':'Flappy Bird is a side-scrolling mobile-game featuring 2D retro style graphics. The objective is to direct a flying bird, named \"Faby\". who moves continuously to the right, between sets of Mario-like-pipes. ',
+						'contacts':'Sneha Pedakam - IIT2013176 - 8953388692<br>Sai Krishna Kasturi - IIT2013085 - 7080329502<br>Aravind V - IIT2013112 - 9807666144'
+					},
+	'Git-Hero':{
+						'rule':'1.The competition will be held at github.<br>2.Redundant commits will carry penalty.<br>3.Use of external tools to edit github history is not allowed.<br>',
+						'description':'Participants would be given a list of repositories to contribute to and the person with : <br>1.Most number of commits<br>2.Best feauture/bug solved.<br>3.First one to solve the problem in our own repository.<br>',
+						'contacts':'Sarthak Panda (IIT2014063)<br>Kaustubh Shamshery (IIT2014147)<br>Naveen Manikanta (IEC2014066)<br>Anujraaj Goel (IIM2014002)'
+					},
+	'Mr-Googler':{
+						'rule':'1. Team of maximum 2 members',
+						'description':'Google...Google...Google and Win<br>How good can you google?',
+						'contacts':'Naz Tabassum (ICM2014504)<br>Upasana Singh (IIT2014053)<br>Rooha Sri (IBM2014046)'
+					},
+	'Mega-IT-Quiz':{
+						'rule':'<u>Round 1 (Pen and Paper Round)</u> 1. Team of maximum 2 members',
+						'description':'The flagship quiz of Aparoksha, Mega IT Quiz boasts of being biggest quizzing event of Aparoksha - The Technical Fest of IIIT-A. With its ever-widening plethora of brain-wrecking questions that leave no geeky-genre untouched, and a maddening, almost ubiquitarian atmosphere where times runs short and questions loom large, Mega IT Quiz gives you a chance to be the cynosure and stun the world with your mettle.<br>Welcome to the pinnacle of quizzing!',
+						'contacts':'Faheem Hassan Zunjani - IIT2015113 - 7828064086<br>Anujraaj Goyal - IIM2014002 - 9999029683<br>G. Pranay Kumar - IIT2014136<br>Neelanjana Jaiswal - ISM2015005<br>Bharat Gupta - IIT2014156'
+					},
+	'BackBone':{
+						'rule':'<ul><li>In case of plagiarism, the team will be disqualified.</li><li>Usage of internet is not allowed.</li><li>The final verdict will be on the discretion of the Organisers, and will be indisputably final.</li><li>Time Allotted will be 4 hours ­ Round 1 ­> 1 hour, Round 2­> 3 Hour</li></ul>',
+						'description':'Design and develop the backend of a webpage and show your skills as a master.The participants have to make a web App with backend in SQL/NoSQL using minimum front end to design of the given topic.There will be given onsite.Rough sketch of the backend should be presented within the first hour.<br>Points:<br><ul><li>creativity : 30</li><li>Response Time : 20</li><li>Extra features: 5 points for each extra substantial features. Maximum marks ­ 30</li><li>Database design:50</li><li>Security:20</li></ul>',
+						'contacts':'<ul><li>Gautham Santhosh : 8281896007</li><li>Ajay pilaniya : 9783123208 </li> <li>Akshat Aggarwal : 9838206921 </li></ul>'
+					},
+	'Pencil-Sketching':{
+						'rule':'<ul><li>Single participant.</li><li>Time:1hour</li><li>We’ll be providing pencils and A4 sheets</li><li>Evaluation criteria:<ul><li>Facebook likes ­ 40%</li><li>organizers ­ 40%</li><li>judges ­ 20%</li></ul></li></ul>',
+						'description':'The participants will be given a theme.All you need to do is make the most creative sketch for the given theme and describe it in a paragraph not more than 100 words.',
+						'contacts':'<ul><li>Sneha Pedakam: 8953388692</li><li>Tarak S: 8181828884</li><li>Bhavana P 8090223073</li><li>Ravi Shankar 9454792569</li></ul>'
+					},
+	'Pool-Runner':{
+						'rule':'<ul><li>The numbers of team members allowed are 2­3.</li><li>You can not use more than 2 actuators.</li><li>The boat should be powered by an onboard battery.</li><li>Driving motor can be water submerged or non­submerged.</li> <li>You are ALLOWED to use pre made wireless modules.</li><li>No other circuitry should be pre­made/pre­assembled.</li><li>No internet controlled modules can be used.</li><li>The time measured by the organizers will be final and will be used for scoring the teams. </li><li>Time measured by any contestant by any other means is not acceptable for scoring.</li><li>Once the boat enters the arena the organizers are not responsible for any kind of malfunction or damage to the boat.</li><li>The event organisers’ decision will be final.</li><li>Dimensions: will be updated soon</li></ul>',
+						'description':'Bring your wireless electronics skills to design a wireless Remote Controlled boat.Being the newest and unique event of the Aparoksha’16, this event is to traverse a given path in a swimming pool. Make your water borne vehicle, a BOAT. Control it through any wireless method, like RF, Bluetooth. Make your run count for best timing avoiding hurdles.<br><b>Requirements (for a demo boat):</b><br><ul><li>RF module</li><li>motors</li><li>battery</li><li>building material</li><li>Place Requirement: Campus Swimming Pool</li><li>Hurdle making materials.</li><li>Other misc. items.</li></ul>',
+						'contacts':'<ul><li>Deepak Singh</li><li>Archi Srivastava</li><li>Lakshmi Likhitha</li><li>Ayush Agarwal</li></ul>'
+					},
+	'Qwerty-Wars':{
+						'rule':'Only individual participation is allowed. Register for the event here (on the spot registration is also accepted.)<br><b><b>Round 1 :</b><br><ul><li>Participants will be required to type the given material as fast as possible.</li><li>There will be no penalty for mistakes( Atleast 70% Accuracy).</li><li>Top scorers will be decided based on their speed in words per minute (WPM).</li></ul><b>Round 2:</b><br><ul><li>Participants will be required to type the given material as fast as possible.</li><li>There will be no penalty for mistakes( Atleast 90% Accuracy).</li><li>Top scorers will be decided based on their speed in words per minute (WPM).</li></ul><b>Round 3:</b><br><ul><li>Participants will be given a code which they will have to type and indent according to the rules given here.</li></ul><br><ul><li>Use of unhealthy practices will result in direct disqualification.</li><li>Proceedings of the events are subject to change without any prior notice.</li><li>Participants would be informed about these changes as soon as possible.</li><li>Software : Typing Master or Type Racer</li></ul>',
+						'description':'How fast you can type??',
+						'contacts':'<ul><li>Swapnil Singh</li><li>Princy Choudhary</li><li>Jatin Goel</li><li>Laxmi</li><li>Praveen Kumar</li><li>Abhinaya Kurnala</li></ul>'
+					},
+	'Riddilonics':{
+						'rule':'<ul><li>There will be two members per group.</li><li> For the second round every group should bring one laptop with installed multisim and ultiboard.</li></ul>',
+						'description':'It is an event which tests your electronics,multisim and circuit making knowledge.<br><ul><li><b>First Round</b><br>we will be conducting a quiz which can be multiple choice or fill in the blank type of questions.</li><li><b>Second Round</b><br>Second round : Short listed students of 1st round will be participating in this and they have to be preparing or editing the multisim file we will be giving you on spot.</li><li><b>Third Round</b><br>Third round : This is the final round and the short listed students of 2nd round will be given a circuit to design on spot.</li></ul>',
+						'contacts':'<ul><li>Puthi Bhavana</li><li>Karada Hareesh</li><li>Sugandh Sagar</li></ul>'
+					},
+	'Tech-Debate':{
+						'rule':'<ul><li><b>Round 1 :</b><br> There will be 2 groups in which 1 group is for and other is against the topic which we will be deciding on the spot and NET will be provided for 10mins for 1st round</li><li><b> Round 2 : </b><br>For the people who are shortlisted will be given individually a topic to speak on and will be given 3mins for the same.No NET access is allowed for this round</li></ul>',
+						'description':' A topic on latest technology will be provided on the spot for group debate.Candidates may use internet for 10 mins to get acquainted with the topic. Topmost candidates will be selected for next round where we will be providing a new topic  to speak on for every person selected.',				'contacts':'<ul><li>Puthi Bhavana</li><li>Aditya Vardhan</li></ul>'
+					},
+	'TechnoFault':{
+						'rule':'Individual event<br>Number of rounds: 3<br>Open to all<br><ul><li><b>1st round:</b><br> Multiple Choice Questions(Qualifier round)<br>Common for all<br>Separate cutoffs for each year<br></li><b>2nd round:</b><br> 1st year ­ Circuit Debugging<br>2nd year ­ Microcontroller code debugging<br>3rd year and rest ­ Verilog code debugging <br></li><li><b>3rd round:</b><br> Debug the given circuit. Implement it on the breadboard and MultiSim<br>Common for all</li></ul>',
+						'description':'TechnoFault is a technical event where you have to find the bugs in the circuits/code.This enthralling event is an excellent platform to prove your passion in this field.Crack your technical brain to unravel the mystery behind the bugs and faulty connections of the circuit.',			
+						'contacts':'<ul><li>K Rakesh Reddy (7275428570)</li><li>T Yashwanth Reddy (8756030427)</li><li>Pankaj Kumawat</li><li>Deepak</li><li>Ravishankar</li><li>Ayush</li></ul>'
+					},
+	'Three-Musketeers':{
+						'rule':'1. You have to come in a team of 3.<br>2. Each team will be assigned with three systems.<br>3. Each member of every team will be given a coding question to be implemented.<br>4. Question for each member in a particular team will be different.<br>4. After a given time (which will be short !), each member will now move to another member\'s (of respective team ) system and complete the code given to him. No communication will be allowed in between.<br>5. If a team member has completed his code in the first go then also he can not announce his code to be completed.<br>',
+						'description':'Can you complete your teammate\'s incomplete code??? Give it a try at this exciting event.Please follow the following rules in exact order of their sequences.<br>Scoring And Evaluation Criteria :<br>For each code successfully compiled (out of 3), the team will be awarded +10 and each test case for which the code gives the correct output the team will be awarded +5.',			
+						'contacts':'<ul><li>K Bhagyaraj (7897009371)</li><li>Osama Siddiqui (8960448482)</li><li>Shivam Goel (8176027469)</li><li>K Sai Krishna Kasturi (7080329502)</li></ul>'
+					},
+	'Wolf-Of-2311':{
+						'rule':'<ul><li>It is a team event and each team will have 2 members. </li><li> There will be three rounds.</li><li>1st  round  is  an  electronics  quiz  that  will  test  your  basic  knowledge  of  hardware.  It  is  a multiple  choice  round  consisting  of  30  questions  and  time  limit  is  60  minutes.  You  will  earn virtual money (points) for each correct answers penalised for wrong.</li><li>2nd round will be circuit designing on the breadboard.Points earned in previous round.</li><li>3rd round consists of real life problem based on circuit design.</li><li>1st prize : Rs. 3000 /­ 2nd prize : Rs. 2000 /­ 3rd prize : Rs. 1000 /­.</li></ul>',
+						'description':'Wolf of 2311 is a team event of electronics hardware design. Test your Technical knowledge with a slight blend of entrepreneurship skills in 2311, the quintessential electronics lab of IIITA.<br><b>3 Rounds : </b><br>1st : 30 MCQ ,60 minutes, max = 150 + 30; top 25% of student ­> next round (1st day 5:00 PM ­ 6:00 PM) Pen and paper. CC­3.<br>2nd : 4 max question ­> 5,10,20,40 points each after 15 minutes. (All hardware design questions) 25% top of student. Each component has differ points. 60 minutes. (2nd day 11:00 AM) CC­1 2311<br>3rd : one real life electronics Question. 30 minutes. (same day 12:00 PM) CC­1 2311.',			
+						'contacts':'<ul><li>Hetiraj Himanshu  - 7376481872</li><li>Shubham Dewangan - 8176027482</li><li>Puja Kumari - 8090297894</li><li>Naveen - 9473726837</li></ul>'
+					},
+
+}
